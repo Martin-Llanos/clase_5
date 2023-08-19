410c2219301179520f48e4582d8088c2a795eba7
Martin-Llanos <112277462+Martin-Llanos@users.noreply.github.com>
2023-08-19 14:36:02 -0300

    Update clase5-tarea1.js

--- clase5-tarea1.js
+++ clase5-tarea1.js
@@ -1,6 +1,6 @@
 const $botonCalcular = document.querySelector("#boton-calcular");
-const $salarioAnual = document.querySelector("#salario-anual");
-const $salarioMensual = document.querySelector("#salario-mensual");
+const $salarioAnual = document.querySelector("245.000");
+const $salarioMensual = document.querySelector("20.416,66");
 
 function calcularSalarioMensual(salarioAnual) {
   if (salarioAnual !== null) {
@@ -12,6 +12,6 @@ function calcularSalarioMensual(salarioAnual) {
 }
 
 $botonCalcular.onclick = function () {
-  $salarioMensual.value = `$ ${calcularSalarioMensual($salarioAnual.value)}`;
+  $salarioMensual.value = `$ ${20.416,66($ 245.000 .value)}`;
   return false;
 };
