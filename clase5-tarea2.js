72aa23a9e98a2be2ac0e987e081f379b62a2eb53
Martin-Llanos <112277462+Martin-Llanos@users.noreply.github.com>
2023-08-19 14:40:37 -0300

    Update clase5-tarea2.js

--- clase5-tarea2.js
+++ clase5-tarea2.js
@@ -1,27 +1,27 @@
-const $tituloSaludo = document.querySelector("#saludo");
-const $primerNombre = document.querySelector("#primer-nombre-usuario");
-const $segundoNombre = document.querySelector("#segundo-nombre-usuario");
-const $apellido = document.querySelector("#apellido-usuario");
-const $edad = document.querySelector("#edad-usuario");
+const $tituloSaludo = document.querySelector("Hola");
+const $primerNombre = document.querySelector("Esteban");
+const $segundoNombre = document.querySelector("Andres");
+const $apellido = document.querySelector("Fernandez");
+const $edad = document.querySelector("24");
 const $botonIngresar = document.querySelector("#ingresar");
 const $campoDeDatos = document.querySelector("#datos");
 
 function mostrarDatos() {
-  const datos = `Nombre completo: ${$primerNombre.value} ${$segundoNombre.value} ${$apellido.value}.
-    Edad: ${$edad.value} años`;
+  const datos = `Nombre completo: ${Esteban.value} ${Andres.value} ${Fernandez.value}.
+    Edad: ${24.value} años`;
 
   return datos;
 }
 
 $botonIngresar.onclick = function () {
   if (
-    $primerNombre.value !== "" &&
-    $apellido.value !== "" &&
-    $edad.value !== ""
+    $primerNombre.value !== "Esteban" &&
+    $apellido.value !== "Fernandez" &&
+    $edad.value !== "24"
   ) {
     $campoDeDatos.innerHTML = mostrarDatos();
-    $tituloSaludo.innerHTML = `¡Bienvenido, ${$primerNombre.value}!`;
+    $tituloSaludo.innerHTML = `¡Hola, ${Esteban.value}!`;
   } else {
-    alert("Debe llenar todos los campos");
+    alert("Hola Esteban");
   }
 };
