edd2cb429623c57ca77534a9d0ecd4afdcc24a96
Martin-Llanos <112277462+Martin-Llanos@users.noreply.github.com>
2023-09-22 16:01:49 -0300

    Add files via upload

---
+++ Clase_5-Tarea.js
@@ -0,0 +1,33 @@
+let funcionesBotones = [habilitarTarea1, habilitarTarea2, habilitarTarea3, habilitarTarea4]
+
+const $botones = document.querySelectorAll(".botones")
+for(let i = 0; i< $botones.length; i++){
+
+  $botones[i].addEventListener("click", funcionesBotones[i]) 
+ 
+  $botones[i].addEventListener("click", lockMenuInicial)
+}
+
+
+function lockMenuInicial(){
+  $botones.forEach(element => {
+    
+    element.setAttribute('disabled', 'disabled');    
+  });
+}
+
+function habilitarTarea1(){
+  window.location = "tarea1.html"
+}
+
+function habilitarTarea2(){
+  window.location = "tarea2.html"
+}
+
+function habilitarTarea3(){
+  window.location = "tarea3.html"
+}
+
+function habilitarTarea4(){
+  window.location = "tarea4.html"
+}
